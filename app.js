74a9ad7395bbc677267a7926467621ffb9edd452
Josh Faigan <djjoker13@gmail.com>
2023-03-29 17:19:04 -0400

    added search form

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.set('view engine', 'handlebars');
 // Routes
 
 app.get('/', (req, res) => {
-  const gifURL = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
-  res.render('hello-gif', {gifURL});
+  console.log(req.query)
+  res.render('home');
 });
 
 app.get('/greetings/:name', (req, res) => {
