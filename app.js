867312f5169e52e6e9b09c687616de238c21646c
Josh Faigan <djjoker13@gmail.com>
2023-03-29 17:13:08 -0400

    adding middleware

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
+
+// Middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
 // Routes
 
 app.get('/', (req, res) => {
-  res.send('Hello Pikachu');
+  const gifURL = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
+  res.render('hello-gif', {gifURL});
+});
+
+app.get('/greetings/:name', (req, res) => {
+  const name = req.params.name;
+  res.render('greetings', {name});
 });
 
 
