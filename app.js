b8aeba4d7bf6e0256babae72337aed1205536444
Josh Faigan <djjoker13@gmail.com>
2023-03-29 18:06:08 -0400

    added working gif search

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const app = express();
+require ('dotenv').config();
+KEY = process.env.API_KEY
+// Tenor
+const Tenor = require("tenorjs").client({
+  // Replace with your own key
+  "Key": KEY, // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
 
 
 // Middleware
@@ -12,8 +20,22 @@ app.set('view engine', 'handlebars');
 // Routes
 
 app.get('/', (req, res) => {
-  console.log(req.query)
-  res.render('home');
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+      // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+      Tenor.Search.Query(term, "10")
+      .then(response => {
+        // store the gifs we get back from the search
+        const gifs = response;
+        // pass the gifs as an object into the home page
+        res.render('home', { gifs })
+      }).catch(console.error);
+    }
+    else {
+      res.render('home', { gifs: [] })
+    }
 });
 
 app.get('/greetings/:name', (req, res) => {
@@ -27,3 +49,4 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
+// AIzaSyAsaHzJNGyyZKpPo1-tfiDEuNH7GCJ3wlA
